7629034294037514b5cdf02a5102731f52741be1
Ervinbara <50152268+Ervinbara@users.noreply.github.com>
2019-07-09 20:46:51 +0200

    Add files via upload

---
+++ public/header.php
@@ -0,0 +1,12 @@
+
+ <header>
+    <figure class="logo"> 
+    <a><img src="images/bicycle.png"  alt="photo à trouvé" /></a> <!--Mettre une photo de l'écrivain ou un truc comme ça-->
+  </figure>
+    <ul class="head">
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="#">Plus d'info</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="admin/index.php">Compte Admin</a></li>
+    </ul> 
+  </header>
