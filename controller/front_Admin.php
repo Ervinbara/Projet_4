65c6f49532076e0d97cb8b423376daa06a169fcd
Ervinbara <50152268+Ervinbara@users.noreply.github.com>
2019-09-03 19:17:15 +0200

    Add files via upload

--- controller/front_Admin.php
+++ controller/front_Admin.php
@@ -9,20 +9,64 @@ require_once ('model/PostManager.php');
 function viewcommentReport(){    
     $adminManager = new AdminManager();
     $comments = $adminManager->get_comment_report();
-    require('admin/admin_view/signaler.php');
+    if($_SESSION){
+        if($_SESSION['username'] == 'admin' AND !empty($_SESSION['username'])){
+            require('admin/admin_view/signaler.php');
+        }
+        else{
+                 header('location: index.php?action=listPosts');
+            }
+    }
+            
+    else{
+             header('location: index.php?action=listPosts');
+        }
 }
 
 function editView(){
     $postManager = new PostManager();
     $post = $postManager->getPost($_GET['id']);
-    require_once('admin/admin_view/edit.php');
+    if($_SESSION){
+        if($_SESSION['username'] == 'admin' AND !empty($_SESSION['username'])){
+            require_once('admin/admin_view/edit.php');
+         }
+        else{
+                 header('location: index.php?action=listPosts');
+            }
+     }
+            
+    else{
+             header('location: index.php?action=listPosts');
+        }
 }
 
 function addchapterView(){
-    require_once('admin/admin_view/creation_chap.php');
+    if($_SESSION){
+        if($_SESSION['username'] == 'admin' AND !empty($_SESSION['username'])){
+            require_once('admin/admin_view/creation_chap.php');
+        }
+        else{
+                 header('location: index.php?action=listPosts');
+            }
+     }
+         else{
+             header('location: index.php?action=listPosts');
+        }
 }
 
 function admin_index_view(){
-    require_once('admin/admin_view/admin_index.php');
+    if($_SESSION){
+        if($_SESSION['username'] == 'admin' AND !empty($_SESSION['username'])){
+            require_once('admin/admin_view/admin_index.php');
+        }
+        elseif(isset($_SESSION['username']) AND !empty($_SESSION['username'])){
+                 header('location: index.php?action=admin_action&alert=connexion_user');
+            }
+     
+    }
+         else{
+             header('location: index.php?action=connexion_View');
+        }
+    
 }
 
