3040f48e5d517b8d6093e8a41c1b2f3ed57ebd69
Ervinbara <50152268+Ervinbara@users.noreply.github.com>
2019-09-03 19:22:32 +0200

    Delete logout.php

--- user/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_SESSION = array();
-session_destroy();
-    echo '<script language="JavaScript" type="text/javascript">
-    window.location.replace("index.php?action=connexion_View&alert=logout");
-    </script>';
-    exit();
-?>
